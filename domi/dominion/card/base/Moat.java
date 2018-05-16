c6e0a8ae42192fba7577da50839334c07de39f7a
MartinClaudel <yornclaudel@gmail.com>
2018-05-16 11:57:51 +0200

    moat

--- domi/dominion/card/base/Moat.java
+++ domi/dominion/card/base/Moat.java
@@ -1,5 +1,6 @@
 package dominion.card.base;
 import java.util.*;
+
 import dominion.*;
 import dominion.card.*;
 
@@ -10,4 +11,8 @@ import dominion.card.*;
  * Lorsqu’un adversaire joue une carte Attaque, vous pouvez dévoiler cette carte de votre main. Dans ce cas, l’Attaque n’a pas d’effet sur vous.
  */
 public class Moat extends ReactionCard {
+
+	public Moat(String name, int cost) {
+		super("Moat", 2);
+	}
 }
