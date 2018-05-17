11483ba68cb6da6eafaf3fb6f5beb280ed5d8e86
thomasbosch <32569802+thomasbosch@users.noreply.github.com>
2018-05-17 18:49:14 +0200

    Update Workshop.java

--- domi/dominion/card/base/Workshop.java
+++ domi/dominion/card/base/Workshop.java
@@ -6,7 +6,21 @@ import dominion.card.*;
 /**
  * Carte Atelier (Workshop)
  * 
- * Recevez une carte coûtant jusqu'à 4 Pièces.
+ * Recevez une carte coÃ»tant jusqu'Ã  4 PiÃ¨ces.
  */
 public class Workshop extends ActionCard {
+	public Workshop(String name, int cost) {
+		super("Workshop", 3);
+	}
+	
+	public void play(Player p) {
+		CardList c= p.getGame().availableSupplyCards();
+		CardList cc= new CardList();
+		Card ccc;
+		while(!c.isEmpty()) {
+			ccc=c.remove(0);
+			if (ccc.getCost()>4) {
+		}
+
+		}
 }
