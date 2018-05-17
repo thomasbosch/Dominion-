85f3ab40234a2e4842b35b198d33a52e60671b16
thomasbosch <32569802+thomasbosch@users.noreply.github.com>
2018-05-17 18:48:12 +0200

    Update Chapel.java

--- domi/dominion/card/base/Chapel.java
+++ domi/dominion/card/base/Chapel.java
@@ -6,7 +6,21 @@ import dominion.card.*;
 /**
  * Carte Chapelle (Chapel)
  * 
- * Écartez jusqu'à 4 cartes de votre main.
+ * Ã‰cartez jusqu'Ã  4 cartes de votre main.
  */
 public class Chapel extends ActionCard {
+	
+	public Chapel(String name, int cost) {
+		super("Chapel", 2);
+	}
+
+	public void play(Player p) {
+			String m="m";
+			int i=4;
+			while(!m.equals("")&&i>0) {
+			m=p.chooseCard("defausse?",p.cardsInHand(),true);
+			p.discard("m");
+			i--;
+			}
+	}
 }
