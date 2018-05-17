978c9254197810d724f0aa2f91b0d4bda8a8df57
thomasbosch <32569802+thomasbosch@users.noreply.github.com>
2018-05-17 09:44:07 +0200

    Update Cellar.java

--- domi/dominion/card/base/Cellar.java
+++ domi/dominion/card/base/Cellar.java
@@ -7,8 +7,23 @@ import dominion.card.*;
  * Carte Cave (Cellar)
  * 
  * +1 Action.
- * Défaussez autant de cartes que vous voulez.
- * +1 Carte par carte défaussée.
+ * DÃ©faussez autant de cartes que vous voulez.
+ * +1 Carte par carte dÃ©faussÃ©e.
  */
 public class Cellar extends ActionCard {
+	public Cellar(String name, int cost) {
+		super("Cellar", 2);
+	}
+	
+	public void play(Player p) {
+		p.incrementActions(1);
+		String m="m";
+		int i=0;
+		while(!m.equals("")) {
+		m=p.chooseCard("defausse?",p.cardsInHand(),true);
+		p.discard("m");
+		i++;
+		}
+		p.drawNCard(i);
+		}
 }
