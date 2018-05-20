9c4b8bf85a0c4c6acd392c387691b49189d1cbfc
thomasbosch <32569802+thomasbosch@users.noreply.github.com>
2018-05-20 18:04:01 +0200

    Add files via upload

--- domi/dominion/card/base/Bureaucrat.java
+++ domi/dominion/card/base/Bureaucrat.java
@@ -10,4 +10,30 @@ import dominion.card.*;
  * Tous vos adversaires dévoilent une carte Victoire et la placent sur leur deck (sinon ils dévoilent leur main afin que vous puissiez voir qu'ils n'ont pas de cartes Victoire).
  */
 public class Bureaucrat extends AttackCard {
+
+	public Bureaucrat() {
+		super("Bureaucrat", 4);
+			}
+
+	@Override
+	public void play(Player p) {
+		p.gaindeck("Silver");
+		
+		for(Player player:p.otherPlayers()) {
+
+
+
+				if(player.getVictoryCards().size()==0) {
+
+							player.revealdeck();
+					}else {
+	
+					String m=player.chooseCard("bbbbbbbbbbbbuu", player.getVictoryCards(), false);
+					player.putondeck(m);
+
+				}
+
+		}
+	}
+				
 }
